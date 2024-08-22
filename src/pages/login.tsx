d2c462d8a3f8805cc374811accb1232f213082c9
Devanshu Kumar <devanshukumar289@gmail.com>
2024-08-22 15:47:38 +0530

    first commit

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,55 +1,54 @@
+// import React, { useState } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { useRouter } from 'next/router';
+// import TextInput from '../components/TextInput';
+// import Button from '../components/Button';
+// import { login } from '../redux/slices/authSlice';
 
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { useRouter } from 'next/router';
-import TextInput from '../components/TextInput';
-import Button from '../components/Button';
-import { login } from '../redux/slices/authSlice';
+// const LoginPage = () => {
+//   const dispatch = useDispatch();
+//   const router = useRouter();
+//   const [credentials, setCredentials] = useState({ username: '', password: '' });
+//   const [errors, setErrors] = useState({});
 
-const LoginPage = () => {
-  const dispatch = useDispatch();
-  const router = useRouter();
-  const [credentials, setCredentials] = useState({ username: '', password: '' });
-  const [errors, setErrors] = useState({});
+//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     const { name, value } = e.target;
+//     setCredentials({ ...credentials, [name]: value });
+//   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setCredentials({ ...credentials, [name]: value });
-  };
+//   const handleSubmit = (e: React.FormEvent) => {
+//     e.preventDefault();
+//     dispatch(login(credentials))
+//       .unwrap()
+//       .then(() => {
+//         router.push('/dashboard');
+//       })
+//       .catch((error) => setErrors(error));
+//   };
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    dispatch(login(credentials))
-      .unwrap()
-      .then(() => {
-        router.push('/dashboard');
-      })
-      .catch((error) => setErrors(error));
-  };
+//   return (
+//     <div className="login-container">
+//       <h1 className="text-2xl font-bold mb-4">Login</h1>
+//       <form onSubmit={handleSubmit}>
+//         <TextInput
+//           label="Username"
+//           name="username"
+//           value={credentials.username}
+//           onChange={handleChange}
+//           error={errors.username}
+//         />
+//         <TextInput
+//           label="Password"
+//           name="password"
+//           type="password"
+//           value={credentials.password}
+//           onChange={handleChange}
+//           error={errors.password}
+//         />
+//         <Button type="submit" label="Login" />
+//       </form>
+//     </div>
+//   );
+// };
 
-  return (
-    <div className="login-container">
-      <h1 className="text-2xl font-bold mb-4">Login</h1>
-      <form onSubmit={handleSubmit}>
-        <TextInput
-          label="Username"
-          name="username"
-          value={credentials.username}
-          onChange={handleChange}
-          error={errors.username}
-        />
-        <TextInput
-          label="Password"
-          name="password"
-          type="password"
-          value={credentials.password}
-          onChange={handleChange}
-          error={errors.password}
-        />
-        <Button type="submit" label="Login" />
-      </form>
-    </div>
-  );
-};
-
-export default LoginPage;
+// export default LoginPage;
